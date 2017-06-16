a363ed744c81abce9411b604dacadbb12b5bd5cc
Lucas Guima <lucasguima.felipe@gmail.com>
2017-06-16 12:04:45 -0300

    Implements the feature of replacement for the challenge

--- src/Challenge.php
+++ src/Challenge.php
@@ -2,17 +2,31 @@
 
 namespace Linio;
 
+use Linio\Replacement;
+
 class Challenge
 {
     public function solve()
     {
+        $replacements = [new Replacement\Linianos(), new Replacement\Linio, new Replacement\IT];
+
         for ($i = 1; $i <= 100; $i++) {
-            echo sprintf('%s ', $this->replace($i));
+            echo sprintf('%s ', $this->replace($i, $replacements));
         }
     }
 
-    public function replace($number)
+    public function replace($number, array $replacements)
     {
+        foreach($replacements as $replacement) {
+            /**
+             * @var $replacement Replacement\Replacement
+             */
+
+            if ($replacement->apply($number)) {
+                return $replacement->value();
+            }
+        }
+
         return $number;
     }
 }
